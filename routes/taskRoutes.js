1d906231fddf4509b2faca76044123dd3abb15e3
Anup-Pokharna-26 <anuppokharna51@gmail.com>
2025-04-12 02:26:06 +0530

    minor updates in date format for manualy add task

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -4,7 +4,7 @@ const {
   createTask,
   getTasks,
   updateTask,
-  deleteTask
+  deleteTask,
 } = require("../controllers/taskController");
 
 // [GET] /api/tasks?date=""&day=""&statusOfDay=""&userId=""
@@ -13,9 +13,16 @@ router.get("/", getTasks); // Fetch tasks based on query parameters
 // [POST] /api/tasks/create -> body (userId, taskDetails)
 router.post("/create", createTask); // Create a new task
 
-// [PUT] /api/tasks/update
-router.put("/update", updateTask); // Update an existing task
+// [PUT] /api/tasks/update -> body (taskId, updateData)
+router.put("/update", (req, res, next) => {
+  console.log("Request to update task:", req.body);
+  next();
+}, updateTask); // Update an existing task
 
-router.delete("/delete", deleteTask);
+// [DELETE] /api/tasks/delete -> body (taskId)
+router.delete("/delete", (req, res, next) => {
+  console.log("Request to delete task:", req.body);
+  next();
+}, deleteTask); // Delete a task
 
 module.exports = router;
